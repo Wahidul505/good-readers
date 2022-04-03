61820a9162b7704f626c3526eb221c8bedb64897
Wahidul30 <wahidul505@gmail.com>
2022-04-04 03:34:56 +0600

    created route for blog component

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -6,7 +6,7 @@ const Blogs = () => {
             <h1>What is Context API ?</h1>
             <p>Context API lets us use data or functionality of a grand parent component or a parent component into child or grand child components without passing the props through components. So Context API avoids the props drilling from component to components and the data or the functionality can be directly passes from a parent component to it's specific child component. From parent component to its child or grand child components, we can pass and use states, functions, event handlers, and so on through Context API.</p>
             <h1>What is Semantic Tag ?</h1>
-            <p>Tags, which clearly describes their purposes are Semantic Tags. A semantic tag clearly describes its meaning both the browser and the developer himself.</p>
+            <p>Tags, which clearly describes their purposes are Semantic Tags. A semantic tag clearly describes its meaning both the browser and the developer himself. Tags, such as  </p>
         </div>
     );
 };
