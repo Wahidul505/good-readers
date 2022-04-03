d41103b1ac0dc59fdcb12175d79d2c98eddc9585
Wahidul30 <wahidul505@gmail.com>
2022-04-04 02:01:28 +0600

    created btn for navigate in reviews + added ratings via stars

--- src/Components/ReviewCart/ReviewCart.js
+++ src/Components/ReviewCart/ReviewCart.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import { FaStar } from 'react-icons/fa';
 
-const ReviewCart = ({review}) => {
-    const {image, name, reviewDetail, ratings} = review;
+const ReviewCart = ({ review }) => {
+    const { image, name, reviewDetail, ratings } = review;
+    let stars;
+    if (ratings === 2) {
+        stars = <><FaStar /><FaStar /></>
+    }
+    else if (ratings === 3) {
+        stars = <><FaStar /><FaStar /><FaStar /></>
+    }
+    else if (ratings === 4) {
+        stars = <><FaStar /><FaStar /><FaStar /><FaStar /></>
+    }
+    else if (ratings === 5) {
+        stars = <><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></>
+    }
     return (
         <div className='text-center p-4 bg-emerald-100 rounded-3xl shadow-xl'>
             <img className='mx-auto rounded-full' src={image} alt="" />
             <p className='text-xl my-2 text-pink-800 font-semibold'>{name}</p>
-            <p className='text-gray-700' title={reviewDetail}>{reviewDetail.length < 200 ? reviewDetail : reviewDetail.slice(0,200) + '...' }</p>
-            <p>Ratings: {ratings}</p>
+            <p className='text-gray-700' title={reviewDetail}>{reviewDetail.length < 200 ? reviewDetail : reviewDetail.slice(0, 200) + '...'}</p>
+            <p className='text-pink-600 flex justify-center my-4'>{stars}</p>
         </div>
     );
 };
--- src/Components/ReviewsDemo/ReviewsDemo.js
+++ src/Components/ReviewsDemo/ReviewsDemo.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useReviews from '../../hooks/useReviews';
 import ReviewCart from '../ReviewCart/ReviewCart';
 
 const ReviewsDemo = () => {
     const [reviews, setReviews] = useReviews();
     const demoReviews = reviews.slice(0,3);
+    const navigate = useNavigate();
     return (
         <div className='p-8'>
             <h1 className='text-3xl text-center text-emerald-600 mb-8'>Reader Reviews</h1>
-            <div className='grid grid-cols-3 gap-6'>
+            <div className='grid grid-cols-3 gap-6 mb-12'>
                 {
                     demoReviews.map(review => <ReviewCart 
                         key={review.id}
@@ -16,6 +18,7 @@ const ReviewsDemo = () => {
                          />)
                 }
             </div>
+            <p className='text-center'><button onClick={()=>navigate('/reviews')} className='text-xl text-white bg-rose-600 rounded-xl p-2'>See all Reviews</button></p>
         </div>
     );
 };
