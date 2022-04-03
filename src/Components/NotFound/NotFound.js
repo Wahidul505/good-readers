63096aab3954ff40a33c67d22b42dc810f041d10
Wahidul30 <wahidul505@gmail.com>
2022-04-04 03:02:00 +0600

    created 404 route + component

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='w-full'>
+            <img className='w-full' src="https://i.pinimg.com/564x/e8/a5/df/e8a5df33d159625fd47c4ce207ad65cb.jpg" alt="" />
         </div>
     );
 };
